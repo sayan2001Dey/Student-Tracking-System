7acbf039d5ab94931feea960c12b79e05619cb71
sayan2001Dey <sayandey450@gnail.com>
2021-12-15 12:11:17 +0530

    1st

--- student_tracking/settings.py
+++ student_tracking/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -148,11 +149,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'static'
-]
+# STATICFILES_DIRS = [ BASE_DIR / 'static']
+
 
 MEDIA_URL = '/media/'
+STATIC_ROOT = os.path.join( BASE_DIR, 'static')
 MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
